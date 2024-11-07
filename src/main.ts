30d746e901a5a4d60cf2016f1086c90e0e2f20af
alexhans <mail@alexanderhans.me>
2024-11-07 23:45:37 +0100

    added express

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,13 @@
-#!/usr/bin/env node
-
-/**
- * This is a sample HTTP server.
- * Replace this with your implementation.
- */
-
 import 'dotenv/config'
-import { createServer, IncomingMessage, ServerResponse } from 'http'
-import { resolve } from 'path'
-import { fileURLToPath } from 'url'
 import { Config } from './config.js'
+import express from 'express'
 
-const nodePath = resolve(process.argv[1])
-const modulePath = resolve(fileURLToPath(import.meta.url))
-const isCLI = nodePath === modulePath
+const app = express()
 
-export default function main(port: number = Config.port) {
-  const requestListener = (request: IncomingMessage, response: ServerResponse) => {
-    response.setHeader('content-type', 'text/plain;charset=utf8')
-    response.writeHead(200, 'OK')
-    response.end('Olá, Hola, Hello!')
-  }
+app.get('/', (_, res: express.Response) => {
+  res.send('Hello, TypeScript with Express!')
+})
 
-  const server = createServer(requestListener)
-
-  if (isCLI) {
-    server.listen(port)
-    // eslint-disable-next-line no-console
-    console.log(`Listening on port: ${port}`)
-  }
-
-  return server
-}
-
-if (isCLI) {
-  main()
-}
+app.listen(Config.port, () => {
+  console.log(`Server is listening on port ${Config.port}`)
+})
